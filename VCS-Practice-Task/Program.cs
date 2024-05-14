8c0ff5773836d54728dab5235787d399317e13bf
SabirMehdiyev <sabirsm@code.edu.az>
2024-05-14 11:38:15 +0400

    Add project files.

---
+++ VCS-Practice-Task/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VCS_Practice_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
